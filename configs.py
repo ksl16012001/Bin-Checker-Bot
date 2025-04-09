5660a6c3a0539bc5dcbfa3c376f52ca5a6851f15
ksl16012001 <phuongthao2k3a@gmail.com>
2025-04-09 14:42:15 +0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,8 +1,8 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
+    API_ID = int(getenv("API_ID", "25831730"))
+    API_HASH = getenv("API_HASH", "92474aac5af1b860d25e3b213a062332")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6734079442:AAGevJ3NspUmkTN6JT027Tyc83MZtmYBwkE")
 
 config = Config()
